d2440aaf3dd11b4ee630e617dcb6b9e79b569670
Zhenyu.Xu <xu_rainy12@hotmail.com>
2019-03-28 23:22:37 +0800

    添加服务

---
+++ serve/server.js
@@ -0,0 +1,53 @@
+
+const Koa = require('koa')
+const next = require('next')
+const logger = require('koa-logger')
+const koabody = require('koa-body')
+const Router = require('koa-router')
+const compress = require('koa-compress')
+
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const env = process.env.NODE_ENV
+const dev = env !== 'production'
+const app = next({ dir: '.', dev })
+
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = new Koa()
+    const router = new Router()
+
+    if ( dev ) {
+      server.use(logger())
+    }
+
+    server.use(koabody())
+    server.use(compress())
+
+    // Default catch-all handler to allow Next.js to handle all other routes
+    router.get('*', async ctx => {
+      await handle(ctx.req, ctx.res)
+      ctx.respond = false
+    })
+
+    server.use(router.routes())
+    server.use(router.allowedMethods())
+
+    server.use((ctx, next) => {
+      ctx.res.statusCode = 200
+      return next()
+    })
+
+    server.listen(port, err => {
+      if (err) {
+        throw err
+      }
+      console.log(`> Ready on port ${port} [${env}]`)
+    })
+  })
+  .catch(err => {
+    console.log('An error occurred, unable to start the server')
+    console.log(err)
+  })
