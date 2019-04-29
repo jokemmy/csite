f89a40534f1287e2e3683630f4f564081253d3b1
Jokemmy <xu_rainy12@qq.com>
2019-04-29 17:33:26 +0800

    解决方案

--- pages/scene/scene1.js
+++ pages/scene/scene1.js
@@ -4,6 +4,7 @@ import classnames from 'classnames';
 import TweenOne from 'rc-tween-one';
 import SvgIcon from '@components/SvgIcon';
 import quote from '@assets/images/scene/quote.svg?sprite';
+import banner from '@assets/images/scene/banner.jpg';
 import styles from './scene1.less';
 
 
@@ -17,35 +18,72 @@ class SectionBlock extends React.Component {
             className={styles.blockBannerMask}
             animation={{ opacity: 0.001, type: 'from', duration: 1000, ease: 'easeOutExpo' }} />
           <div className={classnames( 'page-content', styles.blockBannerContent )}>
-            <h1 className="SceneZcoolGaoDuanHei"><i>智慧能源</i><span className="SceneZcoolXiaoWeiTi">解决方案</span></h1>
+            <h1 className="SceneZcoolGaoDuanHei">
+              <i>智慧能源</i>
+              <span className="SceneZcoolXiaoWeiTi">解决方案</span>
+            </h1>
           </div>
         </section>
         <section className={styles.blockContent}>
           <div className="page-content">
             <blockquote className={styles.dictum}>
-              <SvgIcon className={styles.dictumQuote} icon={quote} />
               <span className={styles.dictumText}>
+                <SvgIcon className={styles.dictumQuote} icon={quote} />
                 数据计算
                 <span className={classnames( 'SceneSiYuan', styles.dictumPoint )}>严谨</span>
                 ，使用方便
                 <span className={classnames( 'SceneSiYuan', styles.dictumPoint )}>简单</span>
-                ，是我们产品的设计宗旨。
+                ，是我们产品的设计宗旨
               </span>
             </blockquote>
-            <p>
-              通过对园区、高等院校、商业建筑、政府机关、大型公共建筑等用户的设施、
-              用能习惯和用能情况进行分析，提供全面的能源管理，实现能源利用最优。
+            <p className="page-poem">
+              江苏欣动信息科技<br />为不同的客户提供定制化的解决方案<br />
+              包括该解决方案针对客户的成本和效果顾虑<br />提供不同的实施方案。
             </p>
-            <p>
-              欣动科技为不同的客户提供定制化的解决方案，包括该解决方案针对客户的成本和效果顾虑，提供不同的实施方案。
+            <p className="page-poem">
+              我们通过<br />对园区、高等院校、商业建筑、政府机关、大型公共建筑等设施<br />
+              用户的用能习惯和用能情况进行分析<br />提供全面的能源管理<br />实现能源利用最优
             </p>
-            <h2 className={styles.creed}>为什么选择我们</h2>
-            为能源计量提供数据基础
-            为节能减排提供管控平台
-            为节能控制提供有效手段
-            为用能决策提供科学依据
-            为科学管理提供技术支撑
-            为统计分析提供科学方法
+          </div>
+        </section>
+        <section className={styles.blockContent}>
+          <div className="page-content">
+            <h2 style={{ textAlign: 'center' }}>方案价值</h2>
+            <div className={styles.worth}>
+              <div className={styles.cardQueue}>
+                <span>为能源计量提供数据基础</span>
+                <span>为节能减排提供管控平台</span>
+                <span>为节能控制提供有效手段</span>
+                <span>为用能决策提供科学依据</span>
+                <span>为科学管理提供技术支撑</span>
+                <span>为统计分析提供科学方法</span>
+              </div>
+              <img alt="" src={banner} width="50%" />
+            </div>
+          </div>
+        </section>
+        <section className={styles.blockContent}>
+          <div className="page-content">
+            <h2 style={{ textAlign: 'center' }}>能源管理从1.0时代迈入2.0时代</h2>
+            <div className={styles.walkForward}>
+              <img alt="" src={banner} height="100%" />
+            </div>
+          </div>
+        </section>
+        <section className={styles.blockContent}>
+          <div className="page-content">
+            <h2 style={{ textAlign: 'center' }}>平台架构</h2>
+            <div className={styles.platform}>
+              <img alt="" src={banner} height="100%" />
+            </div>
+          </div>
+        </section>
+        <section className={styles.blockContent}>
+          <div className="page-content">
+            <h2 style={{ textAlign: 'center' }}>平台架构</h2>
+            <div className={styles.platform}>
+              <img alt="" src={banner} height="100%" />
+            </div>
           </div>
         </section>
       </div>
