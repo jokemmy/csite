6371b142c3cbd30628ace032462bc69dc194ea63
Jokemmy <xu_rainy12@qq.com>
2019-05-06 17:40:38 +0800

    公安备案号

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,7 @@
 
+import moment from 'moment';
 import classnames from 'classnames';
+import police from '@assets/images/icon-police.png';
 import styles from './footer.less';
 
 
@@ -18,7 +20,18 @@ function Footer({ transparent, overlay, mode }) {
         </section>
         <section className="footer-legal">
           <div className="footer-legal-copyright">
-            Copyright © 2019 江苏欣动信息科技有限公司 | 苏ICP备sssssssss号-1
+            Copyright © {moment().year()} 江苏欣动信息科技有限公司
+            <div className="footer-beian">
+              <a target="_blank" href="http://www.beian.miit.gov.cn">苏ICP备19019187号-1</a>
+              <span style={{ width: 300, display: 'inline-block', paddingLeft: 8 }}>
+                <a
+                  target="_blank"
+                  href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32021402001107">
+                  <img alt="police" src={police} style={{ marginRight: 3 }} />
+                  <span style={{ height: 20, lineHeight: '20px' }}>苏公网安备 32021402001107号</span>
+                </a>
+            </span>
+            </div>
           </div>
           <div className="footer-legal-links">
             <a href="#">隐私政策</a>
