50c2a53298b10a40018f728793d29b81bf631a52
jokemmy <252133226@qq.com>
2019-06-29 11:40:02 +0800

    去掉next.zip

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,15 @@ import App, { Container } from 'next/app';
 import Layout from '@layouts/Layout';
 
 
+const fake = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36';
+
 class CustomApp extends App {
 
   static async getInitialProps({ Component, ctx }) {
 
     let pageProps = {};
-    const userAgent = ctx.req ? ctx.req.headers['user-agent'] : navigator.userAgent;
+    let userAgent = ctx.req ? ctx.req.headers['user-agent'] : navigator.userAgent;
+    userAgent = userAgent || fake;
 
     if ( Component.getInitialProps ) {
       pageProps = await Component.getInitialProps( ctx );
