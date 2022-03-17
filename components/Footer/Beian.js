55a7fe709751b0947661c061aa98f79fa994a11e
Zhenyu.Xu <xu_rainy12@qq.com>
2022-03-17 11:45:22 +0800

    修改备案地址

--- components/Footer/Beian.js
+++ components/Footer/Beian.js
@@ -8,11 +8,11 @@ function Beian({ className, ...props }) {
   const { env } =  useContext( ThemeContext );
   return env.pc ? (
     <div {...props} className={classnames( 'footer-beian', className )}>
-      <a target="_blank" href="http://www.beian.miit.gov.cn">苏ICP备19019187号-1</a>
+      <a target="_blank" href="https://beian.miit.gov.cn">苏ICP备19019187号-1</a>
       <span style={{ width: 300, display: 'inline-block', paddingLeft: 8 }}>
         <a
           target="_blank"
-          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32021402001107">
+          href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32021402001107">
           <img alt="police" src={police} style={{ marginRight: 3, width: 12, verticalAlign: 'text-bottom' }} />
           <span style={{ height: 20, lineHeight: '20px' }}>苏公网安备 32021402001107号</span>
         </a>
